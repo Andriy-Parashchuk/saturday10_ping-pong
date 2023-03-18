b42c04233f44f22289ed9af25b5a463f7d1e599c
AndriyParashchyk <60139444+Andriy-Parashchuk@users.noreply.github.com>
2023-03-18 12:00:17 +0200

    Add GameSprite class for future objects
    Change window mode to resizable
    Create ball object
    Add texsture for ball

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 from pygame import *
 
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, sprite_image, x, y, width, height, speed):
+        super().__init__()
+        self.image = transform.scale(image.load(sprite_image), (width, height))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.speed = speed
+
+    def reset(self):
+        virtual_surface.blit(self.image, (self.rect.x, self.rect.y))
+
+
 WIDTH = 1200
 HEIGHT = 700
 
@@ -9,10 +23,15 @@ fps = 60
 back = (106, 245, 168)
 
 window = display.set_mode((WIDTH, HEIGHT), RESIZABLE)
-window.fill(back)
 display.set_caption("Ping-pong")
 
+virtual_surface = Surface((WIDTH, HEIGHT))
+virtual_surface.fill(back)
+current_size = window.get_size()
+
 game = True
+
+ball = GameSprite("ball.png", 500, 300, 200, 200, 10)
 
 while game:
     for e in event.get():
@@ -21,7 +40,13 @@ while game:
         if e.type == KEYDOWN:
             if e.key == K_ESCAPE:
                 game = False
+        if e.type == VIDEORESIZE:
+            current_size = window.get_size()
 
-    window.fill(back)
+    virtual_surface.fill(back)
+    ball.reset()
+
+    scaled_surface = transform.scale(virtual_surface, current_size)
+    window.blit(scaled_surface, (0, 0))
     display.update()
     clock.tick(fps)
